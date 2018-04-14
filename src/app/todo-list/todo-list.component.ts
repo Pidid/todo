c58796d6f070ce3f07a4557814fa854fcedd1ae6
Parker Stromberg <parker@gurutechnologies.net>
2018-04-13 20:15:52 -0600

    Added basic addition and removal behavior with tasks.

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./todo-list.component.css']
 })
 export class TodoListComponent implements OnInit {
+	static latestId = 0;
 	tasks: Task[];
 
 	constructor() { }
@@ -13,16 +14,41 @@ export class TodoListComponent implements OnInit {
 	ngOnInit() {
 		this.tasks = [
 			<Task>{
+				id: TodoListComponent.getId(),
 				name: "Test1"
 			},	
 			<Task>{
+				id: TodoListComponent.getId(),
 				name: "Test2"
 			}	
 		]
 	}
 
+	addTask() {
+		this.tasks.push(<Task>{
+			id: TodoListComponent.getId(),
+			name: ""
+		});
+	}
+
+	removeTask(id: number) {
+		var task = this.tasks.find(task => task.id == id);
+		var taskIndex = this.tasks.indexOf(task);
+		if(task)
+			this.tasks.splice(taskIndex, 1);
+	}
+
+	completeTask(id: number) {
+
+	}
+
+	static getId() {
+		return TodoListComponent.latestId++;
+	}
+
 }
 
 interface Task {
+	id: number;
 	name: string;
 }
